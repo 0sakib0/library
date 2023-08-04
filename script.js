5b522d16c6569aca6c515fda337eab7b4f1edeec
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2023-08-04 22:20:10 +0600

    add initial function skeleton

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("mango");
+const addBookButton = document.querySelector('.book-btn')
+
+let myLibrary = [];
+
+
+function Book() {
+
+}
+
+
+function addBookLibrary() {
+
+}
+
+
+addBookButton.addEventListener("click", () => {
+  console.log(addBookButton);
+})
