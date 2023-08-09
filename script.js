7f9ceec51c05ef35f361edc5ee9811ef546b56c3
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2023-08-09 12:29:08 +0600

    Implement event listener to recieve form data

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const addBookButton = document.querySelector('.book-btn')
-const cancelBtn = document.querySelector('.cancel')
+const addBookButton = document.querySelector('.book-btn');
+const cancelBtn = document.querySelector('.cancel');
+const formContainer = document.querySelector('.form-container');
+const form = document.getElementById('book-form')
 
 let myLibrary = [];
 
 
-function Book() {
-
+function Book(title, author, pages, read) {
+ this.title;
+ this.author;
+ this.pages;
+ this.read;
 }
 
 
@@ -16,13 +21,35 @@ function addBookLibrary() {
 
 
 addBookButton.addEventListener('click', () => {
-  const form = document.getElementById('book-form')
+  
 
   if (form.style.display === 'none') {
-    form.style.display = 'block';
+    form.style.display = 'flex';
+    formContainer.style.backgroundColor = '#265f5f'
+    formContainer.style.borderBottom ='2px solid #ccc923'
   } 
   cancelBtn.addEventListener('click', () => {
     form.style.display = 'none';
+    formContainer.style.backgroundColor = '';
+    formContainer.style.border = 'none';
   })
 })
+
+const formValidator = () => {
+ 
+}
+
+document.getElementById('submit').addEventListener('click', function() {
+  form.style.display = 'none';
+    formContainer.style.backgroundColor = '';
+    formContainer.style.border = 'none';
+  event.preventDefault();
+  
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').value;
+
+  console.log({title, author, pages, read});
+});
 
